0fd3b78483b291daef093ea1ffa6c5af398b73eb
ToKolob <anolis27@hotmail.com>
2025-01-11 19:03:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
    res.send('Hello World, olha nois de novo chachando!');
 })
 
+app.get('/nunes', function (req, res) {
+  res.send('My name is Nunes');
+})
+
 var server = app.listen(3000, function () {
    console.log("Express App running at http://127.0.0.1:5000/");
 })
